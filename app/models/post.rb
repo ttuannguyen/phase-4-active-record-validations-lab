f8ca2c8bccdbcad454bb5d3d089a378bf30f5523
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-10-03 18:08:51 -0400

    commit tree a99ebf04c0769b142665ed8770ff4e60f085721c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    #Post category is either Fiction or Non-Fiction
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
 end
